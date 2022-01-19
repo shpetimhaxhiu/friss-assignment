8a80f88db0a5d071cc6ea7b68f8f1204531a1797
Shpetim Haxhiu <shpetim.h@gmail.com>
2022-01-19 21:50:59 +0100

    Created new formula creation form with validation

--- src/components/FormulaForm.vue
+++ src/components/FormulaForm.vue
@@ -1,13 +1,259 @@
 <template>
-    <div class="container mt-3" id="formulas-form">
-        <h2>Formulas Form</h2>
-
-    </div>
+  <div class="container mt-3" id="formulas-form">
+    <h2 class="mb-5">Create new Formula</h2>
+    <form @submit.prevent @submit="checkForm">
+        <div class="mb-3 row">
+        <p class="col-8">
+            Use the form below to a create new risk calculation and categorization based on vehicle parameters. <br><span v-bind:class="{'text-danger' : formError}">Please note that you must select at least one parameter!</span>
+        </p>
+        </div>
+      <div class="mb-3 row">
+        <label
+          for="exampleInputEmail1"
+          class="form-label col-sm-2 col-form-label"
+          >Make Name</label
+        >
+        <div class="col-sm-6">
+          <select
+            v-bind:makes="makes"
+            class="form-select"
+            aria-label="Default select example"
+            @change="selectMake($event)"
+          >
+            <option selected="selected" value="0">Any make</option>
+            <option
+              v-for="make in makes"
+              :key="make.MakeId"
+              :value="make.MakeId"
+            >
+              {{ make.MakeName }}
+            </option>
+          </select>
+          <div id="emailHelp" class="form-text">
+            Select vehicle manufacturer name.
+          </div>
+        </div>
+      </div>
+      <div class="mb-3 row">
+        <label
+          for="exampleInputEmail1"
+          class="form-label col-sm-2 col-form-label"
+          >Model Name</label
+        >
+        <div class="col-sm-6">
+          <select
+            v-bind:models="models"
+            class="form-select"
+            aria-label="Default select example"
+            @change="selectModel($event)"
+          >
+            <option selected="selected" value="0">Any model</option>
+            <option
+              v-for="model in models"
+              :key="model.Model_ID"
+              :value="model.Model_ID"
+            >
+              {{ model.Model_Name }}
+            </option>
+          </select>
+          <div id="emailHelp" class="form-text">Select vehicle model name.</div>
+        </div>
+      </div>
+      <div class="mb-3 row">
+        <label
+          for="exampleInputEmail1"
+          class="form-label col-sm-2 col-form-label"
+          >Production Year</label
+        >
+        <div class="col-sm-6">
+          <div class="input-group d-flex">
+            <select
+              class="form-select flex-grow-1"
+              aria-label="Default select example"
+              style="width: 30%"
+              v-model="comparisonType"
+            >
+              <option value="0">Any Comparison Type</option>
+              <option value=">">&#62; (Greater then)</option>
+              <option value="<">&#60; (Less Than)</option>
+              <option value="=">&#61; (Equal to)</option>
+            </select>
+            <select class="form-control" v-model="year">
+                <option value="0">Any Year</option>
+              <option v-for="year in years" :key="year" :value="year">
+                {{ year }}
+              </option>
+            </select>
+            <!-- <input type="number" min="1900" max="2099" step="1" value="2016" /> -->
+          </div>
+          <div id="emailHelp" class="form-text">
+            Select vehicle production year and comparison type.
+          </div>
+        </div>
+      </div>
+      <div class="mb-3 row">
+        <label
+          for="exampleInputEmail1"
+          class="form-label col-sm-2 col-form-label"
+          >Fuel Type</label
+        >
+        <div class="col-sm-3">
+          <div class="form-check">
+            <input
+              v-model="fuelType"
+              class="form-check-input"
+              type="checkbox"
+              value="diesel"
+              id="diesel"
+            />
+            <label class="form-check-label" for="diesel"> Diesel </label>
+          </div>
+          <div class="form-check">
+            <input
+              v-model="fuelType"
+              class="form-check-input"
+              type="checkbox"
+              value="petrol"
+              id="petrol"
+            />
+            <label class="form-check-label" for="petrol"> Petrol </label>
+          </div>
+          <div class="form-check">
+            <input
+              v-model="fuelType"
+              class="form-check-input"
+              type="checkbox"
+              value="electric"
+              id="electric"
+            />
+            <label class="form-check-label" for="electric"> Electric </label>
+          </div>
+        </div>
+      </div>
+      
+      <div class="mb-3 row">
+        <label
+          for="exampleInputEmail1"
+          class="form-label col-sm-2 col-form-label"
+          >Risk</label
+        >
+        <div class="col-sm-6">
+          <div class="form-check">
+            <input v-model="risk"
+              class="form-check-input"
+              type="radio"
+              name="low"
+              id="low"
+              value="low"     
+            />
+            <label class="form-check-label" for="low">
+              Low
+            </label>
+          </div>
+          <div class="form-check">
+            <input
+             v-model="risk"
+              class="form-check-input"
+              type="radio"
+              name="medium"
+              id="medium"
+              
+              value="medium"     
+              checked="checked"
+            />
+            <label class="form-check-label" for="medium">
+              Medium
+            </label>
+          </div>
+          <div class="form-check">
+            <input
+             v-model="risk"
+              class="form-check-input"
+              type="radio"
+              name="high"
+              id="high" 
+              value="high"             
+            />
+            <label class="form-check-label" for="high">
+              High
+            </label>
+          </div>
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary">Create</button>
+    </form>
+  </div>
 </template>
 
 
 <script>
+import axios from "axios";
+
+// axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
 export default {
-    name: 'FormulaForm',
-}
+  name: "FormulaForm",
+  data() {
+    return {
+        formError: false,
+      makes: [],
+      models: [],
+      selectedMake: "0",
+      selectedModel: "0",
+      comparisonType: "0",
+      year: 0,
+      fuelType: [],
+      risk: "medium",
+    };
+  },
+  created() {
+    axios
+      .get(
+        "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json"
+      )
+      .then((response) => {
+        this.makes = response.data.Results;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+  computed: {
+    years() {
+      const year = new Date().getFullYear();
+      return Array.from(
+        { length: year - 1900 },
+        (value, index) => 1901 + index
+      );
+    },
+  },
+  methods: {
+    selectMake(event) {
+      this.selectedMake = event.target.value;
+      axios
+        .get(
+          "https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeId/" +
+            this.selectedMake +
+            "?format=json"
+        )
+        .then((response) => {
+          this.models = response.data.Results;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+    selectModel(event) {
+      this.selectedModel = event.target.value;
+    },
+    checkForm(){
+        if(this.selectedMake === "0" && this.selectedModel === "0" && this.year === 0 && this.fuelType.length === 0) {
+            this.formError = true;
+        } else {
+            this.formError = false;
+            alert("Form ready to be submitted");
+        }
+    }
+  },
+};
 </script>
