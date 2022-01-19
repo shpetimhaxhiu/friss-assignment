da31bf3771762ffd0bd526bf80bba2a8c53a33b7
Shpetim Haxhiu <shpetim.h@gmail.com>
2022-01-19 15:24:16 +0100

    Created NavBar Component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <NavBar />
+    <router-view />
   </div>
 </template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+export default {
+  name: "App",
+  components: {
+    NavBar,
+  },
+};
+</script>
