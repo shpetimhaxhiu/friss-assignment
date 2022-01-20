308bbdea7d7e2bd86a5261f08142df1532a39364
Shpetim Haxhiu <shpetim.h@gmail.com>
2022-01-20 15:48:22 +0100

    Fixed hamburger menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -15,8 +15,8 @@
       >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
-        <ul class="navbar-nav">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto">
           <li class="nav-item">
             <router-link class="nav-link" to="/formulas">List of Formulas</router-link>
           </li>
