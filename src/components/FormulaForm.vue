ff901f71c362cd2b01681b407326a9166c4bfe3e
Shpetim Haxhiu <shpetim.h@gmail.com>
2022-01-21 04:55:11 +0100

    Better alerts

--- src/components/FormulaForm.vue
+++ src/components/FormulaForm.vue
@@ -1,195 +1,193 @@
 <template>
-
-    <form @submit.prevent @submit="checkForm">
-      <div class="mb-3 row">
-        <p class="col-8">
-          Use the form below to a create new risk calculation and categorization
-          based on vehicle parameters. <span
-            >Please note that you must select at least one parameter!</span
-          >
-              <b-alert
-            :show="dismissCountDown"
-      
-      variant="danger"
-      class="mt-4"
-      @dismissed="dismissCountDown=0"
-      @dismiss-count-down="countDownChanged"
-    >
-      You must select at least one parameter!
-    </b-alert>
-        </p>
-      </div>
-      <div class="mb-3 row">
-        <label
-          for="exampleInputEmail1"
-          class="form-label col-sm-2 col-form-label"
-          >Make Name</label
+  <form @submit.prevent @submit="checkForm">
+    <div class="mb-3 row">
+      <p class="col-8">
+        Use the form below to a create new risk calculation and categorization
+        based on vehicle parameters.
+        <span>Please note that you must select at least one parameter!</span>
+        <b-alert
+          :show="dismissCountDown"
+          variant="danger"
+          class="mt-4"
+          @dismissed="dismissCountDown = 0"
+          @dismiss-count-down="countDownChanged"
         >
-        <div class="col-sm-6">
-          <select
-            v-model="selectedMake"
-            class="form-select"
-            aria-label="Default select example"
-            @change="selectMake($event)"
+          You must select at least one parameter!
+        </b-alert>
+        <b-alert
+          :show="dismissCountDownSuccess"
+          dissmissable
+          variant="success"
+          class="mt-4"
+          @dismissed="dismissCountDownSuccess = 0"
+          @dismiss-count-down="2"
+
+        >
+          Your rule has been created!
+        </b-alert>
+      </p>
+    </div>
+    <div class="mb-3 row">
+      <label for="selectedMake" class="form-label col-sm-2 col-form-label"
+        >Make Name</label
+      >
+      <div class="col-sm-6">
+        <select
+          v-model="selectedMake"
+          class="form-select"
+          @change="selectMake($event)"
+          id="selectedMake"
+        >
+          <option selected="selected" value="0">Any make</option>
+          <option
+            v-for="make in makes"
+            :key="make.MakeId"
+            :value="make.MakeId"
+            v-bind:data-makename="make.MakeName"
           >
-            <option selected="selected" value="0">Any make</option>
-            <option
-              v-for="make in makes"
-              :key="make.MakeId"
-              :value="make.MakeId"
-              v-bind:data-makename="make.MakeName"
-            >
-              {{ make.MakeName }}
+            {{ make.MakeName }}
+          </option>
+        </select>
+        <div id="selectedMake" class="form-text">
+          Select vehicle manufacturer name.
+        </div>
+      </div>
+    </div>
+    <div class="mb-3 row">
+      <label for="selectedModel" class="form-label col-sm-2 col-form-label"
+        >Model Name</label
+      >
+      <div class="col-sm-6">
+        <select
+          v-model="selectedModel"
+          class="form-select"
+          aria-label="Default select example"
+          @change="selectModel($event)"
+          id="selectedModel"
+        >
+          <option selected="selected" value="0">Any model</option>
+          <option
+            v-for="model in models"
+            :key="model.Model_ID"
+            :value="model.Model_ID"
+            v-bind:data-modelname="model.Model_Name"
+          >
+            {{ model.Model_Name }}
+          </option>
+        </select>
+        <div id="selectedModel" class="form-text">Select vehicle model name.</div>
+      </div>
+    </div>
+    <div class="mb-3 row">
+      <label for="year" class="form-label col-sm-2 col-form-label"
+        >Production Year</label
+      >
+      <div class="col-sm-6">
+        <div class="input-group d-flex">
+          <select
+            class="form-select flex-grow-1"
+            aria-label="Default select example"
+            style="width: 30%"
+            v-model="comparisonType"
+          >
+            <option value="0">Any Comparison Type</option>
+            <option value=">">&#62; (Greater then)</option>
+            <option value="<">&#60; (Less Than)</option>
+            <option value="=">&#61; (Equal to)</option>
+          </select>
+          <select class="form-control" v-model="year" id="year">
+            <option value="0">Any Year</option>
+            <option v-for="year in years" :key="year" :value="year">
+              {{ year }}
             </option>
           </select>
-          <div id="emailHelp" class="form-text">
-            Select vehicle manufacturer name.
-          </div>
+          <!-- <input type="number" min="1900" max="2099" step="1" value="2016" /> -->
+        </div>
+        <div id="emailHelp" class="form-text">
+          Select vehicle production year and comparison type.
         </div>
       </div>
-      <div class="mb-3 row">
-        <label
-          for="exampleInputEmail1"
-          class="form-label col-sm-2 col-form-label"
-          >Model Name</label
-        >
-        <div class="col-sm-6">
-          <select
-            v-model="selectedModel"
-            class="form-select"
-            aria-label="Default select example"
-            @change="selectModel($event)"
-          >
-            <option selected="selected" value="0">Any model</option>
-            <option
-              v-for="model in models"
-              :key="model.Model_ID"
-              :value="model.Model_ID"
-              v-bind:data-modelname="model.Model_Name"
-            >
-              {{ model.Model_Name }}
-            </option>
-          </select>
-          <div id="emailHelp" class="form-text">Select vehicle model name.</div>
+    </div>
+    <div class="mb-3 row">
+      <label for="fuelType" class="form-label col-sm-2 col-form-label"
+        >Fuel Type</label
+      >
+      <div class="col-sm-3">
+        <div class="form-check">
+          <input
+            v-model="fuelType"
+            class="form-check-input"
+            type="checkbox"
+            value="diesel"
+            id="diesel"
+          />
+          <label class="form-check-label" for="diesel"> Diesel </label>
+        </div>
+        <div class="form-check">
+          <input
+            v-model="fuelType"
+            class="form-check-input"
+            type="checkbox"
+            value="petrol"
+            id="petrol"
+          />
+          <label class="form-check-label" for="petrol"> Petrol </label>
+        </div>
+        <div class="form-check">
+          <input
+            v-model="fuelType"
+            class="form-check-input"
+            type="checkbox"
+            value="electric"
+            id="electric"
+          />
+          <label class="form-check-label" for="electric"> Electric </label>
         </div>
       </div>
-      <div class="mb-3 row">
-        <label
-          for="exampleInputEmail1"
-          class="form-label col-sm-2 col-form-label"
-          >Production Year</label
-        >
-        <div class="col-sm-6">
-          <div class="input-group d-flex">
-            <select
-              class="form-select flex-grow-1"
-              aria-label="Default select example"
-              style="width: 30%"
-              v-model="comparisonType"
-            >
-              <option value="0">Any Comparison Type</option>
-              <option value=">">&#62; (Greater then)</option>
-              <option value="<">&#60; (Less Than)</option>
-              <option value="=">&#61; (Equal to)</option>
-            </select>
-            <select class="form-control" v-model="year">
-              <option value="0">Any Year</option>
-              <option v-for="year in years" :key="year" :value="year">
-                {{ year }}
-              </option>
-            </select>
-            <!-- <input type="number" min="1900" max="2099" step="1" value="2016" /> -->
-          </div>
-          <div id="emailHelp" class="form-text">
-            Select vehicle production year and comparison type.
-          </div>
-        </div>
-      </div>
-      <div class="mb-3 row">
-        <label
-          for="exampleInputEmail1"
-          class="form-label col-sm-2 col-form-label"
-          >Fuel Type</label
-        >
-        <div class="col-sm-3">
-          <div class="form-check">
-            <input
-              v-model="fuelType"
-              class="form-check-input"
-              type="checkbox"
-              value="diesel"
-              id="diesel"
-            />
-            <label class="form-check-label" for="diesel"> Diesel </label>
-          </div>
-          <div class="form-check">
-            <input
-              v-model="fuelType"
-              class="form-check-input"
-              type="checkbox"
-              value="petrol"
-              id="petrol"
-            />
-            <label class="form-check-label" for="petrol"> Petrol </label>
-          </div>
-          <div class="form-check">
-            <input
-              v-model="fuelType"
-              class="form-check-input"
-              type="checkbox"
-              value="electric"
-              id="electric"
-            />
-            <label class="form-check-label" for="electric"> Electric </label>
-          </div>
-        </div>
-      </div>
+    </div>
 
-      <div class="mb-3 row">
-        <label
-          for="exampleInputEmail1"
-          class="form-label col-sm-2 col-form-label"
-          >Risk</label
-        >
-        <div class="col-sm-6">
-          <div class="form-check">
-            <input
-              v-model="risk"
-              class="form-check-input"
-              type="radio"
-              name="low"
-              id="low"
-              value="low"
-            />
-            <label class="form-check-label" for="low"> Low </label>
-          </div>
-          <div class="form-check">
-            <input
-              v-model="risk"
-              class="form-check-input"
-              type="radio"
-              name="medium"
-              id="medium"
-              value="medium"
-              checked="checked"
-            />
-            <label class="form-check-label" for="medium"> Medium </label>
-          </div>
-          <div class="form-check">
-            <input
-              v-model="risk"
-              class="form-check-input"
-              type="radio"
-              name="high"
-              id="high"
-              value="high"
-            />
-            <label class="form-check-label" for="high"> High </label>
-          </div>
+    <div class="mb-3 row">
+      <label for="risk" class="form-label col-sm-2 col-form-label"
+        >Risk</label
+      >
+      <div class="col-sm-6">
+        <div class="form-check">
+          <input
+            v-model="risk"
+            class="form-check-input"
+            type="radio"
+            name="low"
+            id="low"
+            value="low"
+          />
+          <label class="form-check-label" for="low"> Low </label>
+        </div>
+        <div class="form-check">
+          <input
+            v-model="risk"
+            class="form-check-input"
+            type="radio"
+            name="medium"
+            id="medium"
+            value="medium"
+          />
+          <label class="form-check-label" for="medium"> Medium </label>
+        </div>
+        <div class="form-check">
+          <input
+            v-model="risk"
+            class="form-check-input"
+            type="radio"
+            name="high"
+            id="high"
+            value="high"
+          />
+          <label class="form-check-label" for="high"> High </label>
         </div>
       </div>
-      <button type="submit" class="btn btn-primary">Create</button>
-    </form>
+    </div>
+    <button type="submit" class="btn btn-primary">Create</button>
+  </form>
 </template>
 
 
@@ -213,8 +211,9 @@ export default {
       year: 0,
       fuelType: [],
       risk: "medium",
-        dismissSecs: 3,
-        dismissCountDown: 0
+      dismissSecs: 3,
+      dismissCountDown: 0,
+      dismissCountDownSuccess: 0,
     };
   },
   created() {
@@ -237,12 +236,18 @@ export default {
     },
   },
   methods: {
+    // alerts methods
     countDownChanged(dismissCountDown) {
-        this.dismissCountDown = dismissCountDown
-      },
-      showAlert() {
-       this.dismissCountDown = this.dismissSecs
-      },
+      this.dismissCountDown = dismissCountDown;
+    },
+    showAlert() {
+      this.dismissCountDown = this.dismissSecs;
+    },
+    showSuccess() {
+      this.dismissCountDownSuccess = this.dismissSecs;
+    },
+
+    // populating makes and models
     selectMake(event) {
       if (event.target.options.selectedIndex > -1) {
         this.selectedMakeName =
@@ -262,7 +267,7 @@ export default {
         .then((response) => {
           this.models = response.data.Results;
         })
-      .catch(this.handleErrors);
+        .catch(this.handleErrors);
     },
     selectModel(event) {
       if (event.target.options.selectedIndex > -1) {
@@ -273,6 +278,7 @@ export default {
       }
       this.selectedModel = event.target.value;
     },
+    // validating form
     checkForm() {
       if (
         this.selectedMake === "0" &&
@@ -280,36 +286,16 @@ export default {
         this.year === 0 &&
         this.fuelType.length === 0
       ) {
-        // this.$bvModal.msgBoxConfirm('Do you really want to delete this rule?', {
-        //   title: 'Please Confirm',
-        //   okVariant: 'danger',
-        //   hideHeaderClose: true,
-        //   centered: true,
-        //   cancelDisabled: true,
-        //   // hide-footer: true
-        // })
-          // .then(value => {
-          //   if(value === true) {
-          //     this.deleteFormula(id);
-          //   }
-          // })
-          // .catch(this.handleErrors)
         this.formError = true;
-        // this.makeToast('danger')
-        this.showAlert()
-        
+        this.showAlert();
       } else {
         this.formError = false;
         this.submitForm();
+        this.showSuccess();
+        // this.succsesfullyCreated = true;
       }
     },
-    makeToast(variant = null) {
-        this.$bvToast.toast('Toast body content', {
-          title: `Variant ${variant || 'default'}`,
-          variant: variant,
-          solid: true
-        })
-      },
+    // submiting form
     submitForm() {
       axios
         .post("http://localhost:3000/formulas", {
@@ -327,21 +313,20 @@ export default {
         })
         .then((response) => {
           if (response.status === 201) {
-            alert("You have succsesfully created new rule!");
+            // alert("You have succsesfully created new rule!");
+
             this.resetForm();
           }
           console.log("response");
         })
-      .catch(this.handleErrors);
+        .catch(this.handleErrors);
     },
     resetForm() {
       this.formError = false;
-      this.makes = [];
-      this.models = [];
       this.selectedMake = "0";
-      this.selectedMakeName = "";
+      this.selectedMakeName = "0";
       this.selectedModel = "0";
-      this.selectedModelName = "";
+      this.selectedModelName = "0";
       this.comparisonType = "0";
       this.year = 0;
       this.fuelType = [];
